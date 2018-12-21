04f864509e768050e10025cdec778f90c7da997a
Derek <derek.baker89@gmail.com>
2018-12-21 11:00:39 +0000

    passed all test again

--- src/set.js
+++ src/set.js
@@ -1,15 +1,29 @@
 export class DerekSet {
   turnListIntoSet(listItems){
     let memory = []
-    console.log('list', listItems);
+    // console.log('list', listItems);
     for (let index = 0; index < listItems.length; index++) {
-      console.log('index', index);
+      // console.log('index', index);
       let item = listItems[index]
       if (!memory.includes(item)){
         memory.push(item);
-        console.log(memory)
       }
     }
     return memory;
   }
+  doesListContainDuplicates(list) {
+    let memo = []
+    for (let index = 0; index < list.length; index++) {
+      let items = list[index];
+
+      if (memo.includes(items)){
+        return true;
+      } else {
+        memo.push(items);
+      }
+
+    }
+    return false;
+  }
 }
+
